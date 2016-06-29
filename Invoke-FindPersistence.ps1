28bdc6f2d1e1399066d16aad641f0108e5244478
Alexander Rymdeko-Harvey <iamfree2009@gmail.com>
2016-06-29 10:24:55 -0700

    IPSubnet

--- Invoke-FindPersistence.ps1
+++ Invoke-FindPersistence.ps1
@@ -2547,7 +2547,10 @@ function Invoke-FindPersitence{
     Jitter for the host delay, defaults to +/- 0-100 milliseconds
 
     .PARAMETER HostList
-    Provid a hostlist one the CLI.
+    Provid a hostlist on the CLI (IP, NAME).
+
+    .PARAMETER IpSubnet
+    Provid a IP with subnet to target.
 
     .PARAMETER OperatingSystem
      Return computers with a specific operating system, wildcards accepted.
@@ -2604,6 +2607,9 @@ function Invoke-FindPersitence{
 
         [Parameter(ValueFromPipeline=$True)]
         $HostList,
+
+        [Parameter(ValueFromPipeline=$True)]
+        $IpSubnet,
 
         [Parameter(ValueFromPipeline=$True)]
         [Int]$TimeOut = 5,
@@ -2668,6 +2674,9 @@ function Invoke-FindPersitence{
         if ($HostList){
             $Computers = $HostList
             Write-Verbose "[*] IP MAIN: hosts passed:"
+        }
+        if ($IpSubnet){
+            $Computers = Get-NetworkRange $IpSubnet
         }
         else {
             # so this isn't repeated if users are passed on the pipeline
@@ -2946,6 +2955,197 @@ function Invoke-FindPersitence{
 #     Helper Calls      #
 #                       #
 #########################
+
+# all subnet math http://www.indented.co.uk/2010/01/23/powershell-subnet-math/
+# all credit to them for that hard work!
+function ConvertTo-Mask {
+  <#
+    .Synopsis
+      Returns a dotted decimal subnet mask from a mask length.
+    .Description
+      ConvertTo-Mask returns a subnet mask in dotted decimal format from an integer value ranging 
+      between 0 and 32. ConvertTo-Mask first creates a binary string from the length, converts 
+      that to an unsigned 32-bit integer then calls ConvertTo-DottedDecimalIP to complete the operation.
+    .Parameter MaskLength
+      The number of bits which must be masked.
+  #>
+  
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+    [Alias("Length")]
+    [ValidateRange(0, 32)]
+    $MaskLength
+  )
+  
+  Process {
+    return ConvertTo-DottedDecimalIP ([Convert]::ToUInt32($(("1" * $MaskLength).PadRight(32, "0")), 2))
+  }
+}
+
+function ConvertTo-DottedDecimalIP {
+  <#
+    .Synopsis
+      Returns a dotted decimal IP address from either an unsigned 32-bit integer or a dotted binary string.
+    .Description
+      ConvertTo-DottedDecimalIP uses a regular expression match on the input string to convert to an IP address.
+    .Parameter IPAddress
+      A string representation of an IP address from either UInt32 or dotted binary.
+  #>
+ 
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+    [String]$IPAddress
+  )
+  
+  process {
+    Switch -RegEx ($IPAddress) {
+      "([01]{8}.){3}[01]{8}" {
+        return [String]::Join('.', $( $IPAddress.Split('.') | ForEach-Object { [Convert]::ToUInt32($_, 2) } ))
+      }
+      "\d" {
+        $IPAddress = [UInt32]$IPAddress
+        $DottedIP = $( For ($i = 3; $i -gt -1; $i--) {
+          $Remainder = $IPAddress % [Math]::Pow(256, $i)
+          ($IPAddress - $Remainder) / [Math]::Pow(256, $i)
+          $IPAddress = $Remainder
+         } )
+       
+        return [String]::Join('.', $DottedIP)
+      }
+      default {
+        Write-Error "Cannot convert this format"
+      }
+    }
+  }
+}
+function ConvertTo-DottedDecimalIP {
+  <#
+    .Synopsis
+      Returns a dotted decimal IP address from either an unsigned 32-bit integer or a dotted binary string.
+    .Description
+      ConvertTo-DottedDecimalIP uses a regular expression match on the input string to convert to an IP address.
+    .Parameter IPAddress
+      A string representation of an IP address from either UInt32 or dotted binary.
+  #>
+ 
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+    [String]$IPAddress
+  )
+  
+  process {
+    Switch -RegEx ($IPAddress) {
+      "([01]{8}.){3}[01]{8}" {
+        return [String]::Join('.', $( $IPAddress.Split('.') | ForEach-Object { [Convert]::ToUInt32($_, 2) } ))
+      }
+      "\d" {
+        $IPAddress = [UInt32]$IPAddress
+        $DottedIP = $( For ($i = 3; $i -gt -1; $i--) {
+          $Remainder = $IPAddress % [Math]::Pow(256, $i)
+          ($IPAddress - $Remainder) / [Math]::Pow(256, $i)
+          $IPAddress = $Remainder
+         } )
+       
+        return [String]::Join('.', $DottedIP)
+      }
+      default {
+        Write-Error "Cannot convert this format"
+      }
+    }
+  }
+}
+function ConvertTo-DecimalIP {
+  <#
+    .Synopsis
+      Converts a Decimal IP address into a 32-bit unsigned integer.
+    .Description
+      ConvertTo-DecimalIP takes a decimal IP, uses a shift-like operation on each octet and returns a single UInt32 value.
+    .Parameter IPAddress
+      An IP Address to convert.
+  #>
+  
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+    [Net.IPAddress]$IPAddress
+  )
+ 
+  process {
+    $i = 3; $DecimalIP = 0;
+    $IPAddress.GetAddressBytes() | ForEach-Object { $DecimalIP += $_ * [Math]::Pow(256, $i); $i-- }
+ 
+    return [UInt32]$DecimalIP
+  }
+}
+function ConvertTo-BinaryIP {
+  <#
+    .Synopsis
+      Converts a Decimal IP address into a binary format.
+    .Description
+      ConvertTo-BinaryIP uses System.Convert to switch between decimal and binary format. The output from this function is dotted binary.
+    .Parameter IPAddress
+      An IP Address to convert.
+  #>
+ 
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+    [Net.IPAddress]$IPAddress
+  )
+ 
+  process {  
+    return [String]::Join('.', $( $IPAddress.GetAddressBytes() |
+      ForEach-Object { [Convert]::ToString($_, 2).PadLeft(8, '0') } ))
+  }
+}
+function ConvertTo-MaskLength {
+  <#
+    .Synopsis
+      Returns the length of a subnet mask.
+    .Description
+      ConvertTo-MaskLength accepts any IPv4 address as input, however the output value 
+      only makes sense when using a subnet mask.
+    .Parameter SubnetMask
+      A subnet mask to convert into length
+  #>
+ 
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Alias("Mask")]
+    [Net.IPAddress]$SubnetMask
+  )
+ 
+  process {
+    $Bits = "$( $SubnetMask.GetAddressBytes() | ForEach-Object { [Convert]::ToString($_, 2) } )" -replace '[\s0]'
+ 
+    return $Bits.Length
+  }
+}
+function Get-NetworkRange( [String]$IP, [String]$Mask ) {
+  if ($IP.Contains("/")) {
+    $Temp = $IP.Split("/")
+    $IP = $Temp[0]
+    $Mask = $Temp[1]
+  }
+ 
+  if (!$Mask.Contains(".")) {
+    $Mask = ConvertTo-Mask $Mask
+  }
+ 
+  $DecimalIP = ConvertTo-DecimalIP $IP
+  $DecimalMask = ConvertTo-DecimalIP $Mask
+  
+  $Network = $DecimalIP -band $DecimalMask
+  $Broadcast = $DecimalIP -bor ((-bnot $DecimalMask) -band [UInt32]::MaxValue)
+ 
+  for ($i = $($Network + 1); $i -lt $Broadcast; $i++) {
+    ConvertTo-DottedDecimalIP $i
+  }
+}
 
 function Test-Wmi {
     <#
